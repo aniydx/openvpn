f1d17cbdec167d129d87f92e99eef4280ea78418
aniydx <47998918+aniydx@users.noreply.github.com>
2022-09-18 16:20:57 +0800

    Delete useradd.sh

--- useradd.sh
+++
@@ -1,3 +0,0 @@
-OVPN_DATA="ovpn"
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it aniydx/openvpn:v1 easyrsa build-client-full ycz nopass
-docker run -v $OVPN_DATA:/etc/openvpn --rm aniydx/openvpn:v1 ovpn_getclient ycz > ycz.ovpn
